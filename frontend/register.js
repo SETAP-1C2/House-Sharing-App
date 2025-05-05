e1042f57afaa41d22cb4605419d664a5d3cd6d4c
up2214717 <up2214717@myport.ac.uk>
2025-05-05 18:18:07 +0100

    Added Password rules to register.js

--- frontend/register.js
+++ frontend/register.js
@@ -21,7 +21,7 @@ function getPassword() {
     return password;
 }
 
-function checkPassword(password) {
+function checkPasswordLength(password) {
     if(password.length < 8){
         
         return false
@@ -31,17 +31,57 @@ function checkPassword(password) {
     }
 }
 
+function checkCharacterMix(password) {
+    if (checkNumber(password)) {
+        if (checkCase(password)) {
+            if (checkSpecialChars(password)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function checkCase(password) {
+    if (password === 
+        password.toUpperCase()||password === 
+        password.toLowerCase()) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function checkNumber(password) {
+    if (/^\d+$/.test(password)) {
+        return false;
+    }
+    return /\d/.test(password);
+}
+
+function checkSpecialChars(password) {
+    const specialChars = /[!@#$%^&*()\-+={}[\]:;"'<>,.?\/|\\]/;
+    if (specialChars.test(password))
+        return true;
+    else
+        return false;
+}
+
 function registerUser() {
     const firstName = getFirstName();
     const lastName = getLastName();
     const email = getEmail();
     const password = getPassword();
-    if (checkPassword(password)){
-        console.log(`${firstName}`);
-        console.log(`${lastName}`);
-        console.log(`${email}`);
-        console.log(`${password}`);
-
+    if (checkPasswordLength(password)){
+        if (checkCharacterMix(password)) {
+            console.log(`${firstName}`);
+            console.log(`${lastName}`);
+            console.log(`${email}`);
+            console.log(`${password}`);
+        }
+        else {
+            console.log("Password must be a mix of Uppercase, Lowercase, Numbers and Special charaacters")
+        }
     }
     else{
         console.log("Password must be at least 8 characters");
