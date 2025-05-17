94c1c9eb78ae2260d711f3566d9c70d4a1d779af
Oliver-code1 <dubemoliver4@gmail.com>
2025-05-17 13:44:03 +0100

    description length validity

--- frontend/createGroup.js
+++ frontend/createGroup.js
@@ -2,15 +2,32 @@
 // CREATE GROUP VALIDATION LOGIC
 // ===============================
 
+//length less than 50
 function isGroupNameValid() {
   const groupName = document.querySelector("#group-name").value;
 
   if (groupName.length > 50) {
-    alert("Group name must be 50 or less characters.");
+    alert("Group name must be less than 50 characters.");
     return false;
   }
   return true;
 }
+
+
+//name less than 50
+function isGroupDescriptionValid() {
+  const groupDesc = document.querySelector("#group-desc").value;
+
+  if (groupDesc.length > 200) {
+    alert("Group description must be less than 200 characters.");
+    return false;
+  }
+  return true;
+}
+
+
+
+
 
 
 
