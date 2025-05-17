f024a31455f45d046e18ad9f3481b63112e50779
Oliver-code1 <dubemoliver4@gmail.com>
2025-05-17 14:27:59 +0100

    id validity still without the database

--- frontend/createGroup.js
+++ frontend/createGroup.js
@@ -26,6 +26,42 @@ function isGroupDescriptionValid() {
 }
 
 
+//characters must be numbers and 5 digits
+function isGroupIdValid() {
+  const groupId = document.querySelector("#group-id").value;
+
+  //must be 5 digits
+  if (groupId.length !== 5) {
+    alert("Group ID must be exactly 5 digits.");
+    return false;
+  };
+
+
+  //characters must be numbers
+  const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  for (let i=0; i<groupId.length; i++){
+      if (!digits.includes(groupId[i])) {
+          alert("Group ID must contain only digits.");
+          return false;
+      };
+  };
+}
+// Uniqueness check: implement this to the databse constrauint, dont forget
+  "DO NOT FORGET"
+  "You shoukd have a database for the check"
+//   for (let i = 0; i < usedGroupIds.length; i++) {
+//     if (groupId === usedGroupIds[i]) {
+//       alert("Group ID already exists. Please choose another.");
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+
+
+
 
 
 
