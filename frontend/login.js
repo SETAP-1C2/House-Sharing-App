9ea2f541a5f480e747a1109f6648a5564b9af65e
Oliver-code1 <dubemoliver4@gmail.com>
2025-05-23 01:38:18 +0100

    login button now connects to the homepage

--- frontend/login.js
+++ frontend/login.js
@@ -6,14 +6,10 @@ function getLoginEmail() {
 }
 
 
-
-
 function getLoginPassword() {
     const loginPassword = document.querySelector("#password").value;
     return loginPassword;
 }
-
-
 
 
 function loginValid(){
@@ -36,32 +32,57 @@ function loginValid(){
 
 
 
-
-
 function loginUser() {
-  if (validateLoginFields()) {
-    const loginEmail = getLoginEmail();
-    const loginPassword = getLoginPassword();
+    if (loginValid()) {
+        const loginEmail = getLoginEmail();
+        const loginPassword = getLoginPassword();
 
-    console.log("Email:", loginEmail);
-    console.log("Password:", loginPassword);
+        console.log("Email:", loginEmail);
+        console.log("Password:", loginPassword);
 
-    window.location.href = "index.html"; // or any other page
-  }
+        window.location.href = "index.html";
+    }
 }
-
-
-
 
 
 function loginUserAttacher() {
-  const button = document.querySelector("#login-btn"); 
-  if (button) {
-    button.addEventListener("click", loginUser);
-  }
+    const button = document.querySelector("#login-btn"); 
+    if (button) {
+        button.addEventListener("click", loginUser);
+    }
 }
 
 loginUserAttacher();
+
+
+// Attach the toggle logic
+function setupLoginEyeToggle() {
+    const toggleBtn = document.querySelector("#toggle-login-password");
+    const passwordField = document.querySelector("#password");
+
+    if (toggleBtn && passwordField) {
+        attachToggleListener(toggleBtn, passwordField);
+    }
+}
+
+// Listener function separated from setup
+function attachToggleListener(button, input) {
+    button.addEventListener("click", function () {
+        togglePasswordVisibility(input);
+    });
+}
+
+// Logic to toggle input type
+function togglePasswordVisibility(input) {
+    if (input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+}
+
+// Run it
+setupLoginEyeToggle();
 
 
 
