49c30373b3fe3d71f1ca86a0e6bee37689289bc9
jfrith04 <jackdpfrith@gmail.com>
2025-05-28 17:38:36 +0100

    Updated server for database functionality and rename svr.js to svr.cjs
    added backend for database implementation

--- svr.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.use(express.static('frontend'));
-
-app.listen(8080, () => {
-    console.log('Server is running on http://localhost:8080');  
-}); 
